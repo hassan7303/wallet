6d9adc24dc1e740e15cbe01c05724b0312dac305
hassan <hassanali7303@gmail.com>
2023-06-22 08:19:57 +0330

    get value inputs

--- public/typescript.js
+++ public/typescript.js
@@ -1,5 +1,10 @@
-var Name = "hassan";
-console.log(Name);
-var inputs = document.querySelectorAll("input").forEach(function (value) {
-    console.log(value);
+"use strict";
+const form = document.querySelector(".new-item-form");
+const type = document.querySelector("#type");
+const Personal_account = document.querySelector("#Personal_account");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(type.value, Personal_account.value, details.value, amount.valueAsNumber);
 });
--- src/typescript.ts
+++ src/typescript.ts
@@ -1,5 +1,16 @@
-const Name: string = "hassan";
-console.log(Name);
-const inputs = document.querySelectorAll("input").forEach((value) => {
-  console.log(value);
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+const type = document.querySelector("#type") as HTMLSelectElement;
+const Personal_account = document.querySelector(
+  "#Personal_account"
+) as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+  console.log(
+    type.value,
+    Personal_account.value,
+    details.value,
+    amount.valueAsNumber
+  );
 });
